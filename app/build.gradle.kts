2ed997d5148df36556988d4d8b2241742f91fd01
Maxim Kvasov <maksim@voodoo.rocks>
2021-11-02 17:45:08 +0200

    Increase version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.pixelcando.fityouroutfit"
         minSdk = 21
         targetSdk = 31
-        versionCode = 4
+        versionCode = 5
         versionName = "1.0"
         resourceConfigurations.add("en")
         resourceConfigurations.add("it")
